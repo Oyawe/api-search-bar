3f50c821c06b0d679e275d96c624826836324514
Oyawe <oyawegoodness@gmail.com>
2023-09-17 20:46:50 -0700

    added an alert dialog

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -1,6 +1,14 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import "./SearchResults.css";
 
 export const SearchResults = ({ result }) => {
-  return <div className="search-result">{result.name}</div>;
+  return (
+    <div
+      className="search-result"
+      onClick={(e) => alert(`You clicked on ${result.name}`)}
+    >
+      {result.name}
+    </div>
+  );
 };
